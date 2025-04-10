96d83e7d84ea620ccfee64e29c4e1174c3cb9704
fiskaal <filipskala@scholastika.cz>
2025-04-11 00:22:58 +0200

    update
    - new character controller
    - new jumping
    - new attacking

--- Assets/Internal-----------------/Scripts/KetraScripts/PlayerMovement.cs
+++ Assets/Internal-----------------/Scripts/KetraScripts/PlayerMovement.cs
@@ -7,16 +7,20 @@ namespace KetraScripts
     {
         public float maxSpeed;
         public float rotSpeed;
-        public float jumpSpeed;
+        public float jumpHeight;
+        public float gravityMultiplier;
         public float ySpeed;
         public float jumpButtonGracePeriod;
         [SerializeField] private CharacterController characterController;
         [SerializeField] private Animator animator;
+        [SerializeField] private Transform cameraTransform;
         private float originalStepOffset;
         private float? lastGroundedTime;
         private float? jumpButtonPressedTime;
         public int jumpCount = 0;
         public int maxJumps = 2;
+        public bool isJumping;
+        public bool isGrounded;
 
         // Start is called before the first frame update
         void Start()
@@ -33,35 +37,68 @@ namespace KetraScripts
             Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
             float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
 
-            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) 
-            {
-                inputMagnitude /= 2;
-            }
+            //if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) 
+            //{
+            //    inputMagnitude /= 2;
+            //}
 
-            animator.SetFloat("InputMagnitude", inputMagnitude, .05f, Time.deltaTime);
+            animator.SetFloat("InputMagnitude", inputMagnitude, .01f, Time.deltaTime);
             float speed = inputMagnitude * maxSpeed;
+            movementDirection = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up) * movementDirection;
             movementDirection.Normalize();
 
-            ySpeed += Physics.gravity.y * Time.deltaTime;
+            float gravity = Physics.gravity.y * gravityMultiplier;
+
+            if (isJumping && ySpeed > 0 && Input.GetButton("Jump") == false)
+            {
+                gravity *= 2;
+            }
+
+            ySpeed += gravity * Time.deltaTime;
 
             if (characterController.isGrounded)
             {
                 lastGroundedTime = Time.time;
                 jumpCount = 0;
                 ySpeed = -5f; // Small downward force to keep grounded
+                animator.SetBool("isGrounded", true);
+                isGrounded = true;
+                animator.SetBool("isJumping", false);
+                isJumping = false;
+                animator.SetBool("isFalling", false);
+            }
+            else
+            {
+                characterController.stepOffset = 0;
+                animator.SetBool("isGrounded", false);
+                isGrounded = false;
+
+                if((isJumping && ySpeed < 0) || ySpeed < -2)
+                {
+                    animator.SetBool("isFalling", true);
+                }
             }
 
             if (Input.GetButtonDown("Jump"))
             {
                 jumpButtonPressedTime = Time.time;
+
                 if (jumpCount < maxJumps)
                 {
-                    ySpeed = jumpSpeed;
+                    ySpeed = Mathf.Sqrt(jumpHeight * -3 * gravity);
+                    animator.SetBool("isJumping", true);
+                    isJumping = true;
                     jumpCount++;
                     jumpButtonPressedTime = null;
                     lastGroundedTime = null;
                 }
             }
+            if(Input.GetButtonDown("Attack") && isGrounded)
+            {
+                
+                animator.SetTrigger("Attacking");
+            }
+           
 
             characterController.stepOffset = characterController.isGrounded ? originalStepOffset : 0;
 

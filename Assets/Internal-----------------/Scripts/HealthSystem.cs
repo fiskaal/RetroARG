9fa3985a5d9ac5278b1e4d7a84376b9ec76aa03e
fiskaal <filipskala@scholastika.cz>
2025-05-30 00:37:07 +0200

    update

--- Assets/Internal-----------------/Scripts/HealthSystem.cs
+++ Assets/Internal-----------------/Scripts/HealthSystem.cs
@@ -85,7 +85,7 @@ public class HealthSystem : MonoBehaviour
     public void DamagePlayer()
     {
         player.gameObject.GetComponent<CharacterController>().Move(checkpoint - transform.position);
-        //player.transform.position = spawnpoint.position;
+        player.transform.position = spawnpoint.position;
         currentHealth -= dealDamage;
         damageSound.Play();
         Debug.Log("HURT!");
